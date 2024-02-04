f7166113cd5ed18914d225e131e18ab975dd194c
Melvin Soldia <melvinsoldia@gmail.com>
2024-02-04 18:32:56 +0800

    react-testing-library wrapper test-utils

---
+++ src/lib/test-utils.tsx
@@ -0,0 +1,15 @@
+import React, { ReactElement } from "react";
+import { render, RenderOptions } from "@testing-library/react";
+import { TooltipProvider } from "~/components/ui/tooltip";
+
+const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+  return <TooltipProvider>{children}</TooltipProvider>;
+};
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, "wrapper">
+) => render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from "@testing-library/react";
+export { customRender as render };
