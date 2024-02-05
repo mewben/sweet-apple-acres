e6ff27747786ced4da6e06253bf31e83f4754afa
Melvin Soldia <melvinsoldia@gmail.com>
2024-02-06 00:32:42 +0800

    remove cart item icon

--- src/components/cart/cart-item.tsx
+++ src/components/cart/cart-item.tsx
@@ -36,7 +36,7 @@ export const CartItem = ({
           </p>
         </div>
         <div className="flex justify-between">
-          <div className="flex items-center rounded-lg border">
+          <div className="flex items-center gap-1 rounded-md border">
             <Button
               variant="ghost"
               size="icon"
@@ -55,12 +55,22 @@ export const CartItem = ({
           </div>
           <div>
             <Button
-              variant="link"
-              size="sm"
-              className="pr-0 opacity-80"
+              variant="outline"
+              size="icon"
               onClick={() => onRemove(item.id)}
             >
-              Remove
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                viewBox="0 0 24 24"
+                height="1em"
+                width="1em"
+              >
+                <path d="M3 6h18M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2" />
+              </svg>
             </Button>
           </div>
         </div>
